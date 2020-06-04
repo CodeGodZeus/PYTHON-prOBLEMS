3a5cca279a7a232fa06eff62824adb7cdf500a4e
CodeGodZeus <63976266+CodeGodZeus@users.noreply.github.com>
2020-06-04 14:31:04 +0530

    Create backspacestring.py

---
+++ backspacestring.py
@@ -0,0 +1,18 @@
+   def backspaceCompare(self, S: str, T: str) -> bool:
+        string1=[]
+        string2=[]
+        for char in S:
+            if char =='#':
+                if len(string1)>=1:
+                    string1.pop(char)
+                else:
+                    string1.append(char)
+        for char in T:
+            if char=='#':
+                 if len(string2)>=1:
+                        string1.pop(char)
+                 
+                 else:
+                    string2.append(char)
+        return string1==string2
+        
